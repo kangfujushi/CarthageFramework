2e1bf340b46f6581b01ba36a57c5caa06fde4b26
kangfujushi <zhaoningdeemail@qq.com>
2018-12-29 16:27:30 +0800

    创建项目

---
+++ FDLayoutPopGesture/FDLayoutPopGesture.h
@@ -0,0 +1,19 @@
+//
+//  FDLayoutPopGesture.h
+//  FDLayoutPopGesture
+//
+//  Created by 赵宁 on 2018/12/29.
+//  Copyright © 2018 赵宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FDLayoutPopGesture.
+FOUNDATION_EXPORT double FDLayoutPopGestureVersionNumber;
+
+//! Project version string for FDLayoutPopGesture.
+FOUNDATION_EXPORT const unsigned char FDLayoutPopGestureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FDLayoutPopGesture/PublicHeader.h>
+
+
