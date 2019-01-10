a81f685df51a323d10208cb43c50f4e149eae44b
kangfujushi <zhaoningdeemail@qq.com>
2019-01-10 10:29:36 +0800

    create object

---
+++ FDLayoutPopGesture/FDLayoutPopGesture.h
@@ -0,0 +1,23 @@
+//
+//  FDLayoutPopGesture.h
+//  FDLayoutPopGesture
+//
+//  Created by 赵宁 on 2018/12/29.
+//  Copyright © 2018 赵宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FDLayoutPopGesture.
+FOUNDATION_EXPORT double FDLayoutPopGestureVersionNumber;
+
+//! Project version string for FDLayoutPopGesture.
+FOUNDATION_EXPORT const unsigned char FDLayoutPopGestureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FDLayoutPopGesture/PublicHeader.h>
+
+#import "UITableView+FDKeyedHeightCache.h"
+#import "UITableView+FDTemplateLayoutCell.h"
+#import "UITableView+FDIndexPathHeightCache.h"
+#import "UITableView+FDTemplateLayoutCellDebug.h"
+#import "UINavigationController+FDFullscreenPopGesture.h"
