0e9f10575974730425fda4988743daa404975622
kangfujushi <zhaoningdeemail@qq.com>
2019-01-24 18:03:24 +0800

    2019-1-24

---
+++ WGPayEffectView/WGPayEffectView.h
@@ -0,0 +1,19 @@
+//
+//  WGPayEffectView.h
+//  WGPayEffectView
+//
+//  Created by 赵宁 on 2019/1/24.
+//  Copyright © 2019 赵宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WGPayEffectView.
+FOUNDATION_EXPORT double WGPayEffectViewVersionNumber;
+
+//! Project version string for WGPayEffectView.
+FOUNDATION_EXPORT const unsigned char WGPayEffectViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WGPayEffectView/PublicHeader.h>
+
+#import "WGPayView.h"
---
+++ WGPayEffectView/WGPayView.m
@@ -0,0 +1,72 @@
+//
+//  WGPayView.m
+//  Jiayou
+//
+//  Created by 赵宁 on 2019/1/21.
+//  Copyright © 2019 赵宁. All rights reserved.
+//
+
+#import "WGPayView.h"
+
+@interface WGPayView ()
+
+@property (weak, nonatomic) IBOutlet UILabel *title;
+@property (weak, nonatomic) IBOutlet UILabel *price;
+@property (weak, nonatomic) IBOutlet UIButton *aliPay;
+@property (weak, nonatomic) IBOutlet UIButton *wechatPay;
+@property (weak, nonatomic) IBOutlet UIView *payView;
+
+@property (nonatomic ,strong) JYSelectBlock block;
+
+@end
+
+@implementation WGPayView
+
++ (void)showPayView:(NSString *)title Price:(CGFloat)price Block:(void(^)(NSInteger selectIndex))block {
+    WGPayView *view = [[NSBundle mainBundle] loadNibNamed:@"WGPayView" owner:nil options:nil].firstObject;
+    
+    view.block = block;
+    view.title.text = title;
+    view.price.text = [NSString format:@"￥%.2f",price];
+    view.payView.alpha = 0;
+    UIView *superView = [CTB getWindow] ;
+    [superView addSubview:view];
+    [view mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(superView);
+    }];
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        view.payView.alpha = 1;
+        [view.payView mas_updateConstraints:^(MASConstraintMaker *make) {
+            make.centerY.equalTo(view);
+        }];
+        [view layoutIfNeeded];
+    }];
+}
+
+#pragma mark --- Actions
+
+- (IBAction)dismiss:(id)sender {
+    [UIView animateWithDuration:0.3 animations:^{
+        self.alpha = 0;
+        [self.payView mas_updateConstraints:^(MASConstraintMaker *make) {
+            make.centerY.equalTo(self).offset((self.bounds.size.height+self.payView.bounds.size.height)/2);
+        }];
+        [self layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        [self removeFromSuperview];
+    }];
+}
+
+- (IBAction)selecAction:(UIButton *)sender {
+    self.aliPay.selected = sender==self.aliPay;
+    self.wechatPay.selected = !self.aliPay.selected;
+}
+
+- (IBAction)confirmAction:(id)sender {
+    if (self.block) self.block(self.wechatPay.selected);
+    [self dismiss:nil];
+}
+
+
+@end
